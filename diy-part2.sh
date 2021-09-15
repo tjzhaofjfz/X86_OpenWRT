8ae7ebb74eff8016bd48fa920e57339f72d97ab6
tjx86 <33371398+tjzhaofjfz@users.noreply.github.com>
2021-09-15 16:01:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.10.253/g' package/base-files/files/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 ZZZ="package/lean/default-settings/files/zzz-default-settings"
